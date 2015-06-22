bc448255f8eceadbfec751617a7a3bd597d62f0d
Dimeo <Dimeo@192.168.0.10>
2015-06-22 19:09:04 +0200

    Waffe_4 fertig geschrieben.

--- src/gui/Launcher.java
+++ src/gui/Launcher.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 import game.Game;
@@ -300,10 +295,6 @@ public class Launcher extends WindowProperties{
     private void beenden() {
         System.exit(0);
     }
-
-    /*
-     Versteckt den Launcher. Soll Später das Spiel beginnen.
-     */
     
 
 }
